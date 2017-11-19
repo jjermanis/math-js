fb6a47963bb693253fb4703f521938362e7d996c
John Jermanis <33067902+jjermanis@users.noreply.github.com>
2017-11-18 20:34:22 -0800

    Added unit tests for primeFactors() (#3)

--- tests/MathModuleSpec.js
+++ tests/MathModuleSpec.js
@@ -39,3 +39,22 @@ describe('MathModule.isPrime', () => {
 
 
 });
+
+
+describe('MathModule.primeFactors', () => {
+    
+    // TODO: build this out.  Match unit tests from math-cs. Use help func?
+    var checkFactors = (num, expected) => {
+        it('should know the prime factors of ' + num, () => {
+            expect(MathModule.primeFactors(num)).toEqual(expected)
+        });
+    };
+    
+    checkFactors(6, {2:1, 3:1});
+    checkFactors(60, {2:2, 3:1, 5:1});
+    checkFactors(13, {13:1});
+    checkFactors(9699690, {2:1, 3:1, 5:1, 7:1, 11:1, 13:1, 17:1, 19:1});
+    checkFactors(2147483647, {2147483647:1});
+    checkFactors(2147483646, {2:1, 3:2, 7:1, 11:1, 31:1, 151:1, 331:1});
+    checkFactors(2147117569, {46337:2});
+});
